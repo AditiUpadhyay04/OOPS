93a9d847631b323bf8b97e0fd662e42b0a13364e
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-11-01 23:16:46 +0530

    Commit

---
+++ src/polymorphism/RunTime/Print.java
@@ -0,0 +1,10 @@
+package polymorphism.RunTime;
+
+public class Print
+{
+    public static void main(String[] args) {
+        Print_number obj1 = new Print_number(100);
+        System.out.println(obj1);
+
+    }
+}
---
+++ src/polymorphism/RunTime/Print_number.java
@@ -0,0 +1,13 @@
+package polymorphism.RunTime;
+
+public class Print_number {
+    int num;
+
+    public Print_number(int num){
+        this.num = num;
+    }
+    public String toString(){
+        return " num = " + this.num ;
+    }
+}
+
