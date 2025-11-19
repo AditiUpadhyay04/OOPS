9024e7da88a1d5ae514f60e5343679927b47f797
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-11-19 23:27:48 +0530

    Commit

---
+++ src/DataHiding.java
@@ -0,0 +1,34 @@
+class BankAccount1 {
+        private double balance;
+
+        // Deposit
+        public void deposit(double amount) {
+            if (amount > 0) {
+                balance += amount;
+            }
+        }
+
+        // Withdraw
+        public void withdraw(double amount) {
+            if (amount > 0 && amount <= balance) {
+                balance -= amount;
+            }
+        }
+
+        // Getter
+        public double getBalance() {
+            return balance;
+        }
+    }
+
+    public class DataHiding {
+        public static void main(String[] args) {
+            BankAccount1 acc = new BankAccount1();
+            acc.deposit(5000);
+            acc.withdraw(1500);
+
+            System.out.println("Balance: " + acc.getBalance());
+        }
+    }
+
+
