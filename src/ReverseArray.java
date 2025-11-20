ef0227f451b2298af30e81d36c2e616e898bf5ac
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-11-20 23:37:12 +0530

    Commit

---
+++ src/ReverseArray.java
@@ -0,0 +1,10 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30, 40};
+
+        System.out.print("Reversed: ");
+        for (int i = arr.length - 1; i >= 0; i--)
+            System.out.print(arr[i] + " ");
+    }
+}
+
